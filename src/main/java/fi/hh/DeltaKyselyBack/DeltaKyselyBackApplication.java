381638980707f18b20412ccf2b0c80ab5e4736f2
TerhiElina <terhi.evakallio@gmail.com>
2024-03-28 11:41:51 +0200

    second commit

---
+++ src/main/java/fi/hh/DeltaKyselyBack/DeltaKyselyBackApplication.java
@@ -0,0 +1,13 @@
+package fi.hh.DeltaKyselyBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaKyselyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeltaKyselyBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/hh/DeltaKyselyBack/DeltaKyselyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.hh.DeltaKyselyBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeltaKyselyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
