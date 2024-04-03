6874a6565946ee92db93c6ca5a543286ddfba885
TerhiElina <terhi.evakallio@gmail.com>
2024-04-03 15:04:46 +0300

    Entity annotaatio lisätty

--- src/main/java/DeltaKyselyBack/domain/Kysymys.java
+++ src/main/java/DeltaKyselyBack/domain/Kysymys.java
@@ -1,9 +1,16 @@
 package DeltaKyselyBack.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+@Entity
 public class Kysymys {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
  private Long kysymysId;
  private String kysymysTeksti;
 
