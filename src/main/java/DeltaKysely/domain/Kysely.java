754f02bc911c7f32f10bd12bfbf767e6e92711bc
anha2 <anja.harju@outlook.com>
2024-04-03 14:27:05 +0300

    Lisätty @Entity, @Id, @GeneratedValue ja @OneToMany

--- src/main/java/DeltaKysely/domain/Kysely.java
+++ src/main/java/DeltaKysely/domain/Kysely.java
@@ -1,53 +1,66 @@
 package DeltaKysely.domain;
 
 import java.util.Date;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Kysely {
-private Long id;
-private String otsikko;
-private Date pvm;
 
-public Kysely() {
-    this.id = null;
-    this.otsikko = null;
-    this.pvm = null;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String otsikko;
+    private Date pvm;
+
+    @OneToMany(mappedBy = "kysely", cascade = CascadeType.ALL)
+    private List<Kysely> kyselyt;
+
+    public Kysely() {
+        this.id = null;
+        this.otsikko = null;
+        this.pvm = null;
+    }
+
+    public Kysely(Long id, String otsikko, Date pvm) {
+        this.id = id;
+        this.otsikko = otsikko;
+        this.pvm = pvm;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOtsikko() {
+        return otsikko;
+    }
+
+    public void setOtsikko(String otsikko) {
+        this.otsikko = otsikko;
+    }
+
+    public Date getPvm() {
+        return pvm;
+    }
+
+    public void setPvm(Date pvm) {
+        this.pvm = pvm;
+    }
+
+    @Override
+    public String toString() {
+        return "Kysely [id=" + id + ", otsikko=" + otsikko + ", pvm=" + pvm + "]";
+    }
+
 }
-
-public Kysely(Long id, String otsikko, Date pvm) {
-    this.id = id;
-    this.otsikko = otsikko;
-    this.pvm = pvm;
-}
-
-public Long getId() {
-    return id;
-}
-
-public void setId(Long id) {
-    this.id = id;
-}
-
-public String getOtsikko() {
-    return otsikko;
-}
-
-public void setOtsikko(String otsikko) {
-    this.otsikko = otsikko;
-}
-
-public Date getPvm() {
-    return pvm;
-}
-
-public void setPvm(Date pvm) {
-    this.pvm = pvm;
-}
-
-@Override
-public String toString() {
-    return "Kysely [id=" + id + ", otsikko=" + otsikko + ", pvm=" + pvm + "]";
-}
-
-
-}
-
