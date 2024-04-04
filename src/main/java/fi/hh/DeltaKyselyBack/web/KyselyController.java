7a117a8a583a936298f55b7af98aaf6d5e9a6fa9
TerhiElina <terhi.evakallio@gmail.com>
2024-04-04 11:52:34 +0300

    TallennusController lisätty

--- src/main/java/fi/hh/DeltaKyselyBack/web/KyselyController.java
+++ src/main/java/fi/hh/DeltaKyselyBack/web/KyselyController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import DeltaKyselyBack.domain.KyselyRepositorio;
+import DeltaKyselyBack.domain.Kysely;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 public class KyselyController {
@@ -20,5 +26,11 @@ public class KyselyController {
 	public String kysely(Model model) {
 	    return "kysely";
 	}
+	@RequestMapping(value = "/tallenna", method = RequestMethod.POST) 
+		public String save(Kysely kysely) {
+		repository.save(kysely);
+		return "redirect:etusivu";
+	}
+	
 
 }
