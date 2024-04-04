d443027b3b57238388292b5a6d860cd7e0b8acf3
jonitirk <jonitirkkonenmusic@gmail.com>
2024-04-04 11:46:47 +0300

    autowired toiminnot lisätty

--- src/main/java/fi/hh/DeltaKyselyBack/web/KyselyController.java
+++ src/main/java/fi/hh/DeltaKyselyBack/web/KyselyController.java
@@ -1,20 +1,62 @@
 package fi.hh.DeltaKyselyBack.web;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import DeltaKyselyBack.domain.Kysely;
+import DeltaKyselyBack.domain.KyselyRepositorio;
+import DeltaKyselyBack.domain.KysymysRepositorio;
+
 
 @Controller
 public class KyselyController {
 	
-	@GetMapping("/etusivu")
-    public String etusivu(Model model) {
+	@Autowired
+	private KyselyRepositorio kyselyRepositorio;
+	
+	@Autowired
+	private KysymysRepositorio kysymysRepositorio;
+	
+	@RequestMapping(value = { "/", "/etusivu" })
+    public String kyselyList(Model model) {
+		
+		List<Kysely> kyselyt = new ArrayList<>();
+		for (Kysely kysely : kyselyRepositorio.findAll()) {
+			kyselyt.add(kysely);
+		}
+		
+		model.addAttribute("kyselyt", kyselyt);
+		
 		return "etusivu";
        
     }
+	
+	/*
+	 	@RequestMapping(value = { "/", "/players" })
+	public String playerList(Model model) {
+		
+		 List<Player> players = new ArrayList<>();
+		    for (Player player : playerRepository.findAll()) {
+		        players.add(player);
+		    }
+
+		Collections.sort(players, Collections.reverseOrder());
+
+		model.addAttribute("players", players);
+
+		return "players";
+	}
+	 */
 	
 	@PostMapping("/kysely")
 	public String kysely(Model model) {
