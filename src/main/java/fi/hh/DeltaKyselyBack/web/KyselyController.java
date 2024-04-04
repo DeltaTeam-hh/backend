7a05064cf2e42ebef9b068e1b37fd881b245d8ed
JariLx <bhd441@myy.haaga-helia.fi>
2024-04-04 11:15:25 +0300

    lisää HTML

--- src/main/java/fi/hh/DeltaKyselyBack/web/KyselyController.java
+++ src/main/java/fi/hh/DeltaKyselyBack/web/KyselyController.java
@@ -3,15 +3,22 @@ package fi.hh.DeltaKyselyBack.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class KyselyController {
 	
-	@GetMapping("/kysely")
-    public String kysely(Model model) {
-		return "kysely";
+	@GetMapping("/etusivu")
+    public String etusivu(Model model) {
+		return "etusivu";
        
     }
+	
+	@PostMapping("/kysely")
+	public String kysely(Model model) {
+	    return "kysely";
+	}
+
 }
