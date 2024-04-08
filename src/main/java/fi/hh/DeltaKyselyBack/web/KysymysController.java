8420559c76027df5a413b88647beac86e586d707
anha2 <anja.harju@outlook.com>
2024-04-08 08:31:47 +0300

    Tuplaimport poistettu kysymyscontroller

--- src/main/java/fi/hh/DeltaKyselyBack/web/KysymysController.java
+++ src/main/java/fi/hh/DeltaKyselyBack/web/KysymysController.java
@@ -1,12 +1,5 @@
 package fi.hh.DeltaKyselyBack.web;
 
-
-import DeltaKyselyBack.domain.KysymysRepositorio;
-
-import DeltaKyselyBack.domain.Kysymys;
-
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
