d89c818323fdc8026f52290518a5665bf9b52404
anha2 <anja.harju@outlook.com>
2024-04-15 10:13:46 +0300

    Corsfilter restcontrollerit

--- src/main/java/fi/hh/DeltaKyselyBack/web/KyselyRestController.java
+++ src/main/java/fi/hh/DeltaKyselyBack/web/KyselyRestController.java
@@ -8,43 +8,44 @@ import org.springframework.stereotype.Controller;
 
 import fi.hh.DeltaKyselyBack.domain.Kysely;
 import fi.hh.DeltaKyselyBack.domain.KyselyRepositorio;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
+@CrossOrigin
+// (origins = "http://localhost:5175")
 
 @Controller
 
 public class KyselyRestController {
-@Autowired
-private KyselyRepositorio kRepositorio;
+  @Autowired
+  private KyselyRepositorio kRepositorio;
 
-// Hakee kaikki kyselyt
-@GetMapping("/kyselyt")
-public @ResponseBody List <Kysely> getKyselyRest() {
-    return (List <Kysely>) kRepositorio.findAll();
-}
+  // Hakee kaikki kyselyt
+  @GetMapping("/kyselyt")
+  public @ResponseBody List<Kysely> getKyselyRest() {
+    return (List<Kysely>) kRepositorio.findAll();
+  }
 
-//Hakee yksittäisen kyselyn
-@GetMapping ("/kyselyt/{id}")
-public @ResponseBody List <Kysely> getKyselyById(@PathVariable("id") Long kyselyId){
-  return kRepositorio.findByKyselyId(kyselyId);  
-}
-@PostMapping("/kyselyt")
-public @ResponseBody Kysely addKysely(@RequestBody Kysely newKysely){
+  // Hakee yksittäisen kyselyn
+  @GetMapping("/kyselyt/{id}")
+  public @ResponseBody List<Kysely> getKyselyById(@PathVariable("id") Long kyselyId) {
+    return kRepositorio.findByKyselyId(kyselyId);
+  }
+
+  @PostMapping("/kyselyt")
+  public @ResponseBody Kysely addKysely(@RequestBody Kysely newKysely) {
     return kRepositorio.save(newKysely);
-}
+  }
 
 }
-/*@PostMapping("/books")
-public @ResponseBody Book addBook(@RequestBody Book newBook){
-   return repository.save(newBook); 
-} */
-
-
-
-
-
+/*
+ * @PostMapping("/books")
+ * public @ResponseBody Book addBook(@RequestBody Book newBook){
+ * return repository.save(newBook);
+ * }
+ */
--- src/main/java/fi/hh/DeltaKyselyBack/web/VastausRestController.java
+++ src/main/java/fi/hh/DeltaKyselyBack/web/VastausRestController.java
@@ -1,15 +1,21 @@
-package fi.hh.DeltaKyselyBack.domain;
+package fi.hh.DeltaKyselyBack.web;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import fi.hh.DeltaKyselyBack.domain.Vastaus;
+import fi.hh.DeltaKyselyBack.domain.VastausRepositorio;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
+@CrossOrigin
+// (origins = "http://localhost:5175")
 
 @RestController
 public class VastausRestController {
@@ -17,18 +23,14 @@ public class VastausRestController {
     @Autowired
     private VastausRepositorio vRepositorio;
 
-    //Haetaan kaikki vastaukset
+    // Haetaan kaikki vastaukset
     @GetMapping("/vastaukset")
-    public @ResponseBody List <Vastaus> getVastausRest() {
-        return (List <Vastaus>) vRepositorio.findAll();
+    public @ResponseBody List<Vastaus> getVastausRest() {
+        return (List<Vastaus>) vRepositorio.findAll();
     }
 
     @PostMapping("/vastaukset")
-        public @ResponseBody Vastaus addVastaus(@RequestBody Vastaus newVastaus){
-            return vRepositorio.save(newVastaus);
-        }
+    public @ResponseBody Vastaus addVastaus(@RequestBody Vastaus newVastaus) {
+        return vRepositorio.save(newVastaus);
     }
-    
-    
-    
-
+}
